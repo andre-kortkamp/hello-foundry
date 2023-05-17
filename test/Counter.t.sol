ca4f9b5cba81771b723db25fe25071e7c2e481ae
andre.kortkamp <andre.kortkamp@genezys.io>
2023-05-17 14:49:51 -0300

    hello foundry

---
+++ test/Counter.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
+
+contract CounterTest is Test {
+    Counter public counter;
+
+    function setUp() public {
+        counter = new Counter();
+
+    }
+
+    function testInc() public {
+        counter.inc();
+        assertEq(counter.count(), 1);
+    }
+
+}
